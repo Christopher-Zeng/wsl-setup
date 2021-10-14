2f4541d0bdc8b082098633377f3129d04fb607b8
Christopher-Zeng <32022231+Christopher-Zeng@users.noreply.github.com>
2021-10-14 18:09:08 -0400

    nvm pls use path variable.

--- apan-capstone.sh
+++ apan-capstone.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# R and RStudio
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+apt-get update && apt-get install -y --no-install-recommends r-base-dev gdebi-core
+wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2021.09.0-351-amd64.deb
+gdebi -n rstudio-server-2021.09.0-351-amd64.deb
 
-# User configuration
-echo "Configure new user."
-read -p "Username: " Username
-adduser $Username
-usermod -aG sudo $Username
-
-# WSL configurations
-echo "Configure WSL"
-echo '[user]' >>./wsl.conf
-echo 'default="$Username' >>./wsl.conf
-sudo mv ./wsl.conf /etc/wsl.conf
-echo "WSL configuration completed"
+# Python and Jupyter
+echo "Install Python dependencies and Jupyter"
+sudo apt-get update && sudo apt-get install -y python3-pip
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
