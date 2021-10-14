8c7440444493b4d1c80d52e23d896c67deba59f1
Christopher-Zeng <32022231+Christopher-Zeng@users.noreply.github.com>
2021-10-14 17:35:23 -0400

    Added simple scripts to copy paste. Changed git config to systemwide.

---
+++ git-clone.sh
@@ -0,0 +1,4 @@
+# Just copy paste to clone this repo
+git clone /mnt/d/wsl/wsl-misc/setup/.git ~/setup
+cd ~/setup
+bash ./ubuntu-dev.sh
---
+++ ubuntu-dev.sh
@@ -0,0 +1,28 @@
+# User configuration
+echo "Configure new user."
+read -p "Username: " Username
+adduser $Username
+usermod -aG sudo $Username
+git clone /mnt/d/wsl/wsl-misc/setup/.git /home/$Username/setup
+
+# WSL configurations
+echo "Configure WSL"
+printf "[user]\ndefault=\"$Username\"\n" >>./wsl.conf
+sudo mv ./wsl.conf /etc/wsl.conf
+echo "WSL configuration completed"
+
+# Dev Essentials
+echo "Install and check C and C++ build tools"
+sudo apt-get update && sudo apt-get install -y build-essential gdb
+echo "Installation completed"
+echo "Configure Git"
+git config --system credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe"
+git config --system user.name 'Christopher-Zeng'
+git config --system user.email '32022231+Christopher-Zeng@users.noreply.github.com'\
+echo "Git configuration completed"
+
+# Clean up
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+sudo apt clean
