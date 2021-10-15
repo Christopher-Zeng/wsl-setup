f7892d596c538a6b216823daaadfb83220c5ff52
Christopher-Zeng <32022231+Christopher-Zeng@users.noreply.github.comecho>
2021-10-15 16:29:24 -0400

    Transfer work out.

--- csci567.sh
+++ csci567.sh
@@ -1,29 +1,70 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -v
+Script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+Script_name="$(basename $BASH_SOURCE)" 
 
-# From ubuntu-dev
-if [$user != "caesarrav"]
-then
-    export Username="caesarrav"
-    bash ./ubuntu-dev.sh
-    su caesarrav -c 'bash ~/setup/csci567.sh'
+# Echo color configurations
+Red=$(tput setaf 1)
+Green=$(tput setaf 2)
+Lime_yellow=$(tput setaf 190)
+Powder_blue=$(tput setaf 153)
+Magenta=$(tput setaf 5)
+Cyan=$(tput setaf 6)
+Bright=$(tput bold)
+Normal=$(tput sgr0)
+Underline=$(tput smul)
+
+# Sudo with password
+alias sudo="echo $sudoPW | sudo -S"
+if [[ "$USER" != "root" && ("$USER" != "$Username" || -z "$sudoPW") ]]
+then 
+    echo "please provide password for $USER to invoke sudo:"
+    read -s -p "password for $USER:" sudoPW
 fi
 
-# Clone the csci567-lab repo
-git clone https://github.com/Christopher-Zeng/csci567-lab.git ~/csci567-lab
+# Execution begins
+echo "${Lime_yellow}Started csci567 script.${Normal}"
 
-# Python and Jupyter
-echo "Install Python dependencies and Jupyter"
-sudo apt-get update && sudo apt-get install -y python3-pip python3-venv
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath
-export PATH="$PATH:/home/caesarrav/.local/bin"
-eval "$(/home/caesarrav/.local/bin/register-python-argcomplete pipx)"
-pipx install virtualenv
+# Check prereqs
+distroUsername="chris-zeng"
+if [ "$USER" != "$distroUsername" ]
+then
+    # From ubuntu-dev image
+    echo "${Green}Prereqs not satisfied.${Normal}"
+    echo "${Green}Build ubuntu-dev image.${Normal}"
+    export Username
+    export sudoPW
+    export distroUsername
+    bash $Script_dir/ubuntu-dev.sh
+    echo "${Green}Build complete: ubuntu-dev.${Normal}"
+    echo "${Green}Switch user to continue execution.${Normal}"
+    sudo su $distroUsername -c "bash /home/$distroUsername/setup/$Script_name"
+else
+    echo "${Green}Prereqs satisfied.${Normal}"
 
-# Setup project virtual environment
-virtualenv ~/csci567-lab/env
-printf "\n# Start up within project virtual environment.\ncd ~/csci567-lab\nsource ./env/bin/activate\n" >> ~/.bashrc
-cd ~/csci567-lab
-source ./env/bin/activate
-python3 -m pip install --upgrade pip
-pip install --upgrade jupyterlab numpy pandas
+    # Python and Jupyter
+    echo "${Green}Install Python dependencies.${Normal}"
+    sudo apt-get update && sudo apt-get install -y python3-pip python3-venv
+    echo "${Green}Install virtualenv.${Normal}"
+    python3 -m pip install --user pipx
+    python3 -m pipx ensurepath
+    export PATH="$PATH:/home/caesarrav/.local/bin"
+    eval "$(/home/caesarrav/.local/bin/register-python-argcomplete pipx)"
+    pipx install virtualenv
+    echo "${Green}Installation completed.${Normal}"
+
+    # Clone the csci567-lab repo
+    echo "${Green}Clone project git repo.${Normal}"
+    git clone https://github.com/Christopher-Zeng/csci567-lab.git ~/csci567-lab
+    echo "${Green}Git repo cloned.${Normal}"
+    # Setup project virtual environment
+    echo "${Green}Setup project environment.${Normal}"
+    virtualenv ~/csci567-lab/env
+    printf "\n# Start up within project virtual environment.\ncd ~/csci567-lab\nsource ./env/bin/activate\n" >> ~/.bashrc
+    source ~/csci567-lab/env/bin/activate
+    python3 -m pip install --upgrade pip
+    pip install --upgrade jupyterlab numpy pandas
+    echo "${Green}Project environment setup completed.${Normal}"
+fi
+
+# Execution ends
+echo "${Lime_yellow}Ended csci567 script.${Normal}"
--- ubuntu-dev.sh
+++ ubuntu-dev.sh
@@ -1,32 +1,69 @@
-# User configuration
-echo "Configure new user."
-if [-z $Username]
+#!/usr/bin/env bash -v
+Script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Echo color configurations
+Red=$(tput setaf 1)
+Green=$(tput setaf 2)
+Lime_yellow=$(tput setaf 190)
+Powder_blue=$(tput setaf 153)
+Magenta=$(tput setaf 5)
+Cyan=$(tput setaf 6)
+Bright=$(tput bold)
+Normal=$(tput sgr0)
+Underline=$(tput smul)
+
+# Sudo with password
+alias sudo="echo $sudoPW | sudo -S"
+if [[ "$USER" != "root" && ("$USER" != "$Username" || -z "$sudoPW") ]]
 then 
-    read -p "Username: " Username
+    read -s -p "please provide password for $USER to invoke sudo:\n" sudoPW
 fi
-adduser $Username
-usermod -aG sudo $Username
-git clone https://github.com/Christopher-Zeng/wsl-setup.git /home/$Username/setup
+
+# Execution begins
+echo "${Lime_yellow}Started ubuntu-dev script.${Normal}"
+
+# User configuration
+echo "${Green}Configure new user.${Normal}"
+if [ -z "$distroUsername" ]
+then 
+    read -p "Username: " distroUsername
+fi
+sudo adduser $distroUsername
+sudo usermod -aG sudo $distroUsername
+# Copy the setup directory to the user home directory
+sudo mkdir /home/$distroUsername/setup
+sudo cp -r $Script_dir/* /home/$distroUsername/setup
+sudo chmod +s $(ls /home/$distroUsername/setup/*.sh)
+echo "${Green}User setup complete.${Normal}"
 
 # WSL configurations
-echo "Configure WSL"
-printf "[user]\ndefault=\"$Username\"\n" >>./wsl.conf
+echo "${Green}Configure WSL settings.${Normal}"
+printf "[user]\ndefault=\"$distroUsername\"\n" >>./wsl.conf
 sudo mv ./wsl.conf /etc/wsl.conf
-echo "WSL configuration completed"
+echo "the content of /etc/wsl.conf is:"
+cat /etc/wsl.conf
+echo "${Green}WSL configuration completed.${Normal}"
 
 # Dev Essentials
-echo "Install and check C and C++ build tools"
-sudo apt-get update && sudo apt-get install -y build-essential gdb
-echo "Installation completed"
-echo "Configure Git"
-##!!!===change here to config git information===!!!##
+echo "${Green}Install dev essentials.${Normal}"
+sudo apt-get update && sudo apt-get install -y build-essential
+echo "${Green}Installation completed.${Normal}"
+echo "${Green}Configure Git settings.${Normal}"
+#!!!===change here to config git information===!!!#
 git config --system credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager-core.exe"
 git config --system user.name 'Christopher-Zeng'
-git config --system user.email '32022231+Christopher-Zeng@users.noreply.github.com'\
-echo "Git configuration completed"
+git config --system user.email '32022231+Christopher-Zeng@users.noreply.github.com'
+echo "the current git configuration is:"
+git config --list
+echo "${Green}Git configuration completed.${Normal}"
 
 # Clean up
+echo "${Green}APT upgrade and cleanup.${Normal}"
 sudo apt update
 sudo apt full-upgrade -y
 sudo apt autoremove -y
 sudo apt clean
+echo "${Green}APT upgraded.${Normal}"
+
+# Execution ends
+echo "${Lime_yellow}Ended ubuntu-dev script.${Normal}"
