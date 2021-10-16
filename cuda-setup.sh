ece19e957f8f457c6378d04d217915b7593ae75b
Christopher-Zeng <32022231+Christopher-Zeng@users.noreply.github.com>
2021-10-15 23:17:21 -0400

    Add cuda setup scripts.

---
+++ cuda-setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+Script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+Script_name="$(basename $BASH_SOURCE)" 
+
+# Echo color configurations
+Red=$(tput setaf 1)
+Green=$(tput setaf 2)
+Lime_yellow=$(tput setaf 190)
+Powder_blue=$(tput setaf 153)
+Magenta=$(tput setaf 5)
+Cyan=$(tput setaf 6)
+Bright=$(tput bold)
+Normal=$(tput sgr0)
+Underline=$(tput smul)
+
+# Execution begins
+echo "${Lime_yellow}Started $Script_name.${Normal}"
+
+# CUDA support
+echo "${Green}Install CUDA toolkits for WSL.${Normal}"
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-wsl-ubuntu-11-4-local/7fa2af80.pub
+sudo apt update && sudo apt -y install cuda
+echo "${Green}Install DBMS backends.${Normal}"
+
+# Execution ends
+echo "${Lime_yellow}Ended $Script_name.${Normal}"
