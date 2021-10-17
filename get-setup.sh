fb3b5ec132ab7a0ae2f2e3afcaa58c87fd03ec55
Christopher-Zeng <32022231+Christopher-Zeng@users.noreply.github.com>
2021-10-16 21:32:34 -0400

    Switch back to github repo.

--- get-setup.sh
+++ get-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Just copy paste to clone this repo
-git clone /mnt/d/wsl/wsl-misc/setup/.git /opt/setup
+git clone https://github.com/Christopher-Zeng/wsl-setup.git /opt/setup
 cd /opt/setup
 chmod a+x ./*.sh
