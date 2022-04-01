79cc3143b91402f158f224ee7ac5bd1cdd83afb6
Christopher-Zeng <32022231+Christopher-Zeng@users.noreply.github.com>
2022-04-01 17:29:22 -0400

    update cuda install script

--- cuda-setup.sh
+++ cuda-setup.sh
@@ -18,12 +18,12 @@ echo "${Lime_yellow}Started $Script_name.${Normal}"
 
 # CUDA support
 echo "${Green}Install CUDA toolkits for WSL.${Normal}"
-wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
-sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
-sudo dpkg -i cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
-sudo apt-key add /var/cuda-repo-wsl-ubuntu-11-4-local/7fa2af80.pub
-sudo apt update && sudo apt -y install cuda
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-ubuntu2004-11-4-local_11.4.0-470.42.01-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-4-local_11.4.0-470.42.01-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-4-local/7fa2af80.pub
+sudo apt-get update && apt-get install -y cuda-toolkit-11-4
 
 # Execution ends
 echo "${Lime_yellow}Ended $Script_name.${Normal}"
