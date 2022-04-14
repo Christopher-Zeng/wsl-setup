8d6bed586f86c9f771259043889fb44bf9afee5e
Christopher-Zeng <32022231+Christopher-Zeng@users.noreply.github.com>
2022-04-14 19:22:55 -0400

    Added conda setup

--- virtualenv-config.sh
+++ virtualenv-config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-Script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-Script_name="$(basename $BASH_SOURCE)" 
+Script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+Script_name="$(basename $BASH_SOURCE)"
 
 # Echo color configurations
 Red=$(tput setaf 1)
@@ -19,10 +19,33 @@ echo "${Lime_yellow}Started $Script_name.${Normal}"
 # Setup Python virtual environment
 echo "${Green}Install Python virtualenv.${Normal}"
 python3 -m pip install --user pipx
-python3 -m pipx ensurepath 
+python3 -m pipx ensurepath
 PATH="$PATH:/home/$distroUsername/.local/bin"
 eval "$(register-python-argcomplete pipx)"
 pipx install virtualenv
+echo "${Green}Installation completed.${Normal}"
+
+# Setup miniconda and mamba
+echo "${Green}Install Miniconda.${Normal}"
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+miniconda.sh -b -p /home/$distroUsername/miniconda
+cat >>~/.bashrc <<"EOF"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/dev/miniconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/dev/miniconda/etc/profile.d/conda.sh" ]; then
+        . "/home/dev/miniconda/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/dev/miniconda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+EOF
 echo "${Green}Installation completed.${Normal}"
 
 # Execution ends
